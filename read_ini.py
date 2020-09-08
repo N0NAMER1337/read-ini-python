685be789f12e0992bb625ce2bf35f7e9c4479ed1
Alexey <paxa026@yandex.ru>
2020-09-08 19:26:05 +0300

    Python Code
    Python Code

--- read_ini.py
+++ read_ini.py
@@ -1,9 +1,9 @@
 from configparser import ConfigParser
 CP = ConfigParser()
 CP.read('ini_file.ini')
-text1 = config['Texts']['Text1']
-text2 = config['Texts']['Text2']
-text3 = config['Texts']['Text3']
+text1 = CP['Texts']['Text1']
+text2 = CP['Texts']['Text2']
+text3 = CP['Texts']['Text3']
 print (text1)
 print (text2)
 print (text3)
