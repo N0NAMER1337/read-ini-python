d88043387b53422718e10374378f77b20c70b0d1
N0NAMER1337 <paxa026@yandex.ru>
2020-09-09 15:02:28 +0300

    Python Code
    Python Code

--- read_ini.py
+++ read_ini.py
@@ -1,10 +1,10 @@
-from configparser import ConfigParser
-CP = ConfigParser()
-CP.read('ini_file.ini')
-text1 = CP['Texts']['Text1']
+from configparser import ConfigParser #Include library
+CP = ConfigParser() 
+CP.read('ini_file.ini') #Read ini file
+text1 = CP['Texts']['Text1'] #Write data from ini file to variables
 text2 = CP['Texts']['Text2']
 text3 = CP['Texts']['Text3']
-print (text1)
+print (text1) #Print variables
 print (text2)
 print (text3)
 print ('Donate: https://www.donationalerts.com/r/n0namer1337')
