a4570464e2ca8f455b10d1fdf3866f494b6f13f6
N0NAMER1337 <paxa026@yandex.ru>
2020-09-09 15:02:50 +0300

    Write to File
    Write to File

--- read_ini_and_write_to_file.py
+++ read_ini_and_write_to_file.py
@@ -1,18 +1,18 @@
-from configparser import ConfigParser
+from configparser import ConfigParser #Include library
 CP = ConfigParser()
-CP.read('ini_file.ini')
-text1 = CP['Texts']['Text1']
+CP.read('ini_file.ini') #Read ini file
+text1 = CP['Texts']['Text1'] #Write data from ini file to variables
 text2 = CP['Texts']['Text2']
 text3 = CP['Texts']['Text3']
-text1_file = open('text1.txt', 'w')
+text1_file = open('text1.txt', 'w') #Create the files
 text2_file = open('text2.txt', 'w')
 text3_file = open('text3.txt', 'w')
 all_in_one = open('all_in_one.txt', 'w')
-text1_file.write(text1)
+text1_file.write(text1) #Write variables to files
 text2_file.write(text2)
 text3_file.write(text3)
 all_in_one.write(text1 + '\n' + text2 + '\n' + text3)
-print (text1)
+print (text1) #Print variables
 print (text2)
 print (text3)
 print ('Donate: https://www.donationalerts.com/r/n0namer1337')
